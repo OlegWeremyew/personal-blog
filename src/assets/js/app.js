8927a6836ec22fe74921292fd0a31448890659e4
olegweremey <olegweremey1994@mail.ru>
2023-01-27 22:16:57 +0300

    feat: added favicon

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -49,3 +49,47 @@ function closeModal(modal) {
     BODY.classList.remove("no-scroll");
   }, 200);
 }
+
+//=================================================
+
+const BURGER = document.getElementById("sidebarToggle");
+const SIDEBAR = document.getElementById("sidebar");
+const PAGE = document.getElementById("page");
+
+BURGER.addEventListener("click", (event) => {
+  if (BODY.classList.contains("show-sidebar")) {
+    closeSidebar();
+    return;
+  }
+  showSidebar();
+});
+
+function showSidebar() {
+  let mask = document.createElement("div");
+  mask.classList.add("page__mask");
+  mask.addEventListener("click", closeSidebar)
+  PAGE.appendChild(mask);
+
+  BODY.classList.add('show-sidebar')
+}
+
+function closeSidebar() {
+  BODY.classList.remove('show-sidebar');
+  const mask = document.querySelector(".page__mask");
+  mask.remove();
+}
+
+//================================================================
+
+const TEXTAREAS = document.querySelectorAll("[data-autoresize]");
+
+TEXTAREAS.forEach((textarea) => {
+  const textareaHeight = textarea.offsetHeight;
+
+  textarea.addEventListener("input", (event) => {
+    let _this = event.target;
+
+    _this.style.height = `${textareaHeight}px`;
+    _this.style.height = `${_this.scrollHeight}px`;
+  });
+});
